6856f10734d2c27c1fa31ae9d88f2a785987a4c2
singsong19 <69883408+singsong19@users.noreply.github.com>
2021-01-06 23:19:59 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,15 +3,24 @@
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;//게임을 얼만큼의 시간동안 할건지 정의
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect(); //field의 전체적인 사이즈와 위치까지 알수 있도록
 const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
-let started = false;
-let score = 0;
-let timer = undefined;
+const popUp = document.querySelector('.pop-up');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
+const popUpText = document.querySelector('.pop-up__message');
+
+
+let started = false; //게임의 상태를 알고있는 변수 (시작)
+let score = 0; //최종 점수기억
+let timer = undefined; //총 얼마만의 시간이 남았는지 기억 초기 시작되지 않았을 때는 undefined
+
+field.addEventListener('click', onFieldClick);
 
 gameBtn.addEventListener('click', () =>{
   if(started){
@@ -19,37 +28,66 @@ gameBtn.addEventListener('click', () =>{
   } else{
       startGame();
   }
-  started = ! started; 
+});
+popUpRefresh.addEventListener('click', ()=> {
+    startGame();
+    hidePopUp();
 });
 
 
 function startGameTimer(second){
-    if(second ==0){
-        timer = null;
-    } else{
-        timer = setTimeout(function(){startGameTimer(second-1) }, 1000);
-    }
+    let remainingTimeSec = GAME_DURATION_SEC;
+    updateTimerText(remainingTimeSec); //시작전에 업데이트
+    timer = setInterval(() => {
+        if(remainingTimeSec <= 0){ //interval이 1초마다 불려질때, 남아있는 시간이 0초보다 같거나 작다면?
+            clearInterval(timer) //진행되지 않도록 
+            finishGame(CARROT_COUNT === score);
+            return;
+        }
+        updateTimerText(--remainingTimeSec) //계속 게임이 돌아가고 있다면, 동일하게 update를 쓰고 그 후에는 
+        //5초로 시작했다면 한바퀴 돌고나서 4초로 바뀌고 .. 하나씩 줄어야함.
+    },1000);
 }
 
+function stopGameTimer(){
+    clearInterval(timer);
+}
 
-
+function updateTimerText(time) {
+    const minutes = Math.floor(time / 60); //floor는 integer함수로 소수점을 내려주는 함수
+    const seconds = time % 60;
+    gameTimer.innerText = `${minutes}:${seconds}`;
+}
 
 function startGame(){
+    started = true;
     initGame();
     showStopButton();
     showTimerAndScore();
     startGameTimer();
 }
-function stopGame(){
 
-        
+function stopGame() {
+    started = false;
+    stopGameTimer();
+    hideGameButton();
+    showPopUpWithText('REPLAY?')
 }
 
+function finishGame(win){
+    started = false;
+    hideGameButton();
+    showPopUpWithText(win? 'YOU WON!!' : 'YOU LOST')
+}
 
 function showStopButton(){
-    const icon = gameBtn.querySelector('.fa-play')
+    const icon = gameBtn.querySelector('.fas')
     icon.classList.add('fa-stop');
     icon.classList.remove('fa-play');
+}
+
+function hideGameButton(){
+    gameBtn.style.visibility = 'hidden';
 }
 
 function showTimerAndScore(){
@@ -57,6 +95,21 @@ function showTimerAndScore(){
     gameScore.style.visibility = 'visible';
 }
 
+function showPopUpWithText(text){
+    popUpText.innerText = text;
+    popUp.classList.remove('pop-up--hide');
+}
+
+function hidePopUp(){
+    popUp.classList.add('pop-up--hide')
+}
+
+function reStartGame(){
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+}
 
 function initGame(){
     field.innerHTML = '';
@@ -66,12 +119,37 @@ function initGame(){
     addItem('bug', BUG_COUNT, 'img/bug.png');
 }
 
-function addItem(className, count, imgPath) {
+function onFieldClick(event){
+    if(!started){
+        return;
+    }
+    const target = event.target;
+    if(target.matches('.carrot')){ //matches css selector가 맞는지 확인하는 함수!
+        target.remove();
+        score++;
+        updateScoreBoard();
+    if(score === CARROT_COUNT){
+        finishGame(true);
+    }
+    }else if(target.matches('.bug')){
+        stopGameTimer();
+        finishGame(false);
+        }
+    } 
+
+
+
+
+function updateScoreBoard(){
+    gameScore.innerText = CARROT_COUNT - score;
+}
+
+function addItem(className, count, imgPath){
     const x1 = 0;
     const y1 = 0;
     const x2 = fieldRect.width - CARROT_SIZE;
     const y2 = fieldRect.height - CARROT_SIZE;
-    for(let i =0 ; i < count ; i++) {
+    for(let i =0 ; i < count ; i++){
         const item = document.createElement('img');
         item.setAttribute('class', className);
         item.setAttribute('src', imgPath)
@@ -84,11 +162,10 @@ function addItem(className, count, imgPath) {
     }
 }
 
-function randomNumber(min, max) {
+function randomNumber(min, max){
    return Math.random() * (max - min) + min;
 }
 
 
 //재생버튼을 누르면 게임이 초기화가 되면서 다시 랜덤되고, 타이머가 시작되는것! 그리고 남은당근 갯수 표기
-
-
+ 
